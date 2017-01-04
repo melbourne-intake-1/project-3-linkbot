b9a53b950180d2ab7e9c999c27a2ef8b341833c5
jfhinchcliffe <hinchy@gmail.com>
2017-01-04 15:17:21 +1100

    add blank routes for link

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.json({ title: 'Welcome to the API index page' });
 });
 
+// Get a single link
+router.get('/:id', function(req, res, next) {
+  res.json({ title: 'Welcome to the API show page' });
+});
+
+// Create a link by posting to / route
+router.post('/', function(req, res, next) {
+  res.json({ title: 'Welcome to the API post page' });
+});
+
+// Update one post
+router.put('/:id', (req, res, next) => {
+  res.json({ title: 'Welcome to the API update page' });
+});
+
+// Delete a post 
+router.delete('/:id', (req, res, next) => {
+  res.json({ title: 'Welcome to the API delete page' });
+});
+
 module.exports = router;
