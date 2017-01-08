29f11e27dd83acdc79afdd034810a68c5a1a0698
jfhinchcliffe <hinchy@gmail.com>
2017-01-09 10:16:30 +1100

    add dynamic preview and styled

--- front-end/src/components/Form.js
+++ front-end/src/components/Form.js
@@ -7,9 +7,30 @@ class Form extends React.Component {
     this.state = {
       title: '',
       body: '',
-      url: ''
+      url: '',
+      titlePreview: 'title preview',
+      bodyPreview: 'body preview',
+      urlPreview: 'url'
     }
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.updateTitle = this.updateTitle.bind(this)
+    this.updateBody = this.updateBody.bind(this)
+    this.updateURL = this.updateURL.bind(this)
+  }
+
+  updateTitle(e){
+    console.log(e)
+    this.setState({titlePreview: e.target.value})
+  }
+
+  updateURL(e){
+    console.log(e)
+    this.setState({urlPreview: e.target.value})
+  }
+
+  updateBody(e){
+    console.log(e)
+    this.setState({bodyPreview: e.target.value})
   }
 
   handleSubmit(e) {
@@ -32,15 +53,20 @@ class Form extends React.Component {
   render() {
     return (
       <div>
+        <p id='preview' >
+        <h4>Preview</h4>
+        <a href={this.state.urlPreview}>{this.state.titlePreview}</a>
+        <p>{this.state.bodyPreview}</p>
+        </p>
         <form onSubmit={this.handleSubmit}>
           <label>Title:
-            <input type="text" ref="title" placeholder="title" />
+            <input onKeyPress={this.updateTitle} type="text" ref="title" placeholder="title" />
           </label>
           <label>Body:  
-            <input type="text" ref="body" placeholder="body" />
+            <input onKeyPress={this.updateBody} type="text" ref="body" placeholder="body" />
           </label>
           <label>URL:
-            <input type="text" ref="url" placeholder="URL" />
+            <input onKeyPress={this.updateURL}  type="text" ref="url" placeholder="URL" />
           </label>
           <input type="submit" value="Submit" />
         </form>
