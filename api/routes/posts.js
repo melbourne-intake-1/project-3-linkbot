fbbbca0278d6c40e2d7649213eadc8c22379bdf3
jfhinchcliffe <hinchy@gmail.com>
2017-01-19 11:31:29 +1100

    added all routes to comments

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const router = express.Router();
+
+const postsRouter = express.Router();
 const Post = require('../models/post');
-const Comment = require('../models/comment');
-const comments = require('./comments');
+
 const commentsRouter = express.Router({mergeParams: true});
+const Comment = require('../models/comment');
+
 const requireAuthorizedUser = require('../middleware/requireAuthorizedUser');
 
-router.use('/:id/comments', commentsRouter)
+postsRouter.use('/:post_id/comments', commentsRouter)
 
 // post index page of all Posts
-router.get('/', function(req, res, next) {
+postsRouter.get('/', function(req, res, next) {
   // An empty find method will return all Posts
   Post.find()
     .populate('_comments')
@@ -22,7 +24,7 @@ router.get('/', function(req, res, next) {
 });
 
 // Get a single Post
-router.get('/:id', function(req, res, next) {
+postsRouter.get('/:post_id', function(req, res, next) {
   // Use params ID to identify a Post
   Post.findById(req.params.id)
     .populate('_comments')
@@ -32,7 +34,7 @@ router.get('/:id', function(req, res, next) {
 });
 
 // Create a post by posting to / route
-router.post('/', function(req, res, next) {
+postsRouter.post('/', function(req, res, next) {
   // Creates a new post from the attributes passed along in the request (req)
     post = new Post;
     post.title = req.body.title;
@@ -48,7 +50,7 @@ router.post('/', function(req, res, next) {
 });
 
 // Update one post
-router.put('/:id', (req, res, next) => {
+postsRouter.put('/:post_id', (req, res, next) => {
   Post.findById(req.params.id)
     .then(post => {
       // checking to see if the params are present, and updating if they are.
@@ -65,8 +67,8 @@ router.put('/:id', (req, res, next) => {
 });
 
 // Delete a post
-router.delete('/:id', (req, res, next) => {
-  Post.findByIdAndRemove(req.params.id)
+postsRouter.delete('/:post_id', (req, res, next) => {
+  Post.findByIdAndRemove(req.params.post_id)
     .then(() => {
       res.json('Post deleted');
     });
@@ -75,41 +77,64 @@ router.delete('/:id', (req, res, next) => {
 // Comment API links 
 // Nested under comment. URL looks like posts/post_id/comments/comment_id_
 commentsRouter.post('/', function(req, res, next) {
-  console.log(req.params.id)
-  //res.json({response: 'hai'})
-  comment = new Comment;
-    comment.content = req.body.content;
-    comment._post = req.params.id
-    // comment.save((err) => {
-    // if (err)
-    //   res.send(err);
-    //   res.json({comment});
-    // });
-    comment.save((err, doc) => {
-        if (err)
-            res.send(err);
-        Post.findByIdAndUpdate(req.params.id,
-            { $push: { _comments: doc._id } },
-            { new: true },
-            (err, post) => {
-                if (err)
-                    res.send(err);
-                res.json({doc});
-            }
-        )
-    });
+  comment = new Comment({
+    content: req.body.content,
+    _post: req.params.post_id
+  })
+  comment.save((err, doc) => {
+    if (err)
+      res.send(err);
+    Post.findByIdAndUpdate(req.params.post_id,
+      { $push: { _comments: doc._id } },
+      { new: true },
+      (err, post) => {
+       if (err)
+        res.send(err);
+        res.json({doc});
+      }
+    )
+  });
+});
+
+commentsRouter.get('/:comment_id', function(req, res, next) {
+  // Use params ID to identify a Post
+  console.log(`COMMENT ${req.params.comment_id} POST ${req.params.post_id}`)
+  Comment.findById(req.params.comment_id)
+    .then(comment => {
+      res.json(comment)
+    })
 });
 
 commentsRouter.get('/', function(req, res, next) {
-  //An empty find method will return all Posts
-  Comment.find()
+  // finds all comments related to preceding post (req.params.id)
+  Comment.find({ _post: req.params.post_id })
     .then(comments => {
       res.json(comments)
   })
   .catch(err => {
     res.json({ message: err.message })
   })
-  // res.json({message: req.params.id})
 });
 
-module.exports = router;
+commentsRouter.put('/:comment_id', (req, res, next) => {
+  Comment.findById(req.params.comment_id)
+    .then(comment => {
+      // checking to see if the params are present, and updating if they are.
+      if (req.body.content) comment.content = req.body.content;
+      comment.save((err) => {
+        if (err)
+          res.send(err);
+        res.json(comment);
+      });
+    });
+});
+
+// Delete a post
+commentsRouter.delete('/:comment_id', (req, res, next) => {
+  Comment.findByIdAndRemove(req.params.comment_id)
+    .then(() => {
+      res.json('Comment deleted');
+    });
+});
+
+module.exports = postsRouter;
