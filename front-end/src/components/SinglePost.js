99f87cc6ea358cb128b962bb76684916fd0ce908
jfhinchcliffe <hinchy@gmail.com>
2017-01-26 13:26:59 +1100

    FRONT END: Cosmetic changes to SinglePost.js

--- front-end/src/components/SinglePost.js
+++ front-end/src/components/SinglePost.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, CardActions, CardHeader, CardTitle} from 'material-ui/Card';
+import {Card, CardHeader, CardTitle, Link, RaisedButton} from 'material-ui/Card';
 import { getPosts, upvote, deletePost } from '../api/apiCall';
 import FontIcon from 'material-ui/FontIcon';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
@@ -19,10 +19,15 @@ export default class SinglePost extends React.Component {
     this.populatePosts = this.populatePosts.bind(this)
     this.upvotePost = this.upvotePost.bind(this)
     this.deletePost = this.deletePost.bind(this)
+    this.goToLink = this.goToLink.bind(this)
     }
 
   componentDidMount() {
     this.populatePosts()
+  }
+
+  goToLink(url){
+    window.location.href=url;
   }
 
   upvotePost(post) {
@@ -70,12 +75,14 @@ export default class SinglePost extends React.Component {
     return (
       <div>
         <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
-          <CardHeader titleStyle={{fontSize: 18}} titleColor="#444" showExpandableButton={true} title={this.props.currentPost.title} subtitle={`${this.props.currentPost.url} - ${this.props.currentPost.votes} votes `} avatar={this.props.currentPost.image} actAsExpander={true}  />
+          <CardHeader titleStyle={{fontSize: 18}} titleColor="#444" showExpandableButton={true} href={this.props.currentPost.url} title={this.props.currentPost.title} subtitle={`${this.props.currentPost.url} - ${this.props.currentPost.votes} votes `} avatar={this.props.currentPost.image} actAsExpander={true}  />
           <CardTitle subtitle={this.props.currentPost.body} expandable={true} style={{backgroundColor:"#efefef"}} />
           <div style={{padding: 10}}>
             <FontIcon onClick={() => this.props.upvotePost(this.props.currentPost)} style={{cursor:'pointer', fontSize: 24, marginRight: 50}} hoverColor='#43c6f9' cursor='pointer' className="material-icons">thumb_up</FontIcon>
-            <FontIcon style={{cursor:'pointer', fontSize: 24, marginBottom: 20}} hoverColor='#ea2a3d' cursor='pointer' className="material-icons" onClick={() => this.props.deletePost(this.props.currentPost)}>delete_forever</FontIcon>
+            <FontIcon onClick={() => this.goToLink(this.props.currentPost.url)} style={{cursor:'pointer', fontSize: 24, marginRight: 50}} hoverColor='#43c6f9' cursor='pointer' className="material-icons">open_in_browser</FontIcon>
+            <FontIcon style={{cursor:'pointer', fontSize: 24, marginBottom: 20}} hoverColor='#ea2a3d' cursor='pointer' className="material-icons"  onClick={() => this.props.deletePost(this.props.currentPost)}>delete_forever</FontIcon>
           </div>
+          
         </Card>
       </div>
     )
