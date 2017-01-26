53f05b7bbb4f0552e8d3547c115a4dfe492e6ee1
Dylan <thommyboy02@gmail.com>
2017-01-26 13:25:19 +1100

    Styling header and footer

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -10,11 +10,15 @@ export default class Header extends React.Component {
     return (
       <div>
         <AppBar
-          style={{width: '100%', backgroundColor: '#0f4970'}}
-          title={<span style={{cursor: 'pointer', letterSpacing: 0.15, marginRight: 80}}>LinkB<i className="material-icons">android</i>t</span>}
-          iconElementLeft={<FlatButton label="Logout" style={{backgroundColor: '#1a77c9', color: '#EEE', marginTop: 5}} />}
+          style={{width: '100%', backgroundColor: '#268eb7'}}
+          title={<div style={{cursor:'pointer', letterSpacing: 4, marginRight: 30}}>
+            LinkBot</div>}
+          iconElementLeft={<i className="material-icons" style={{color:"#EEE", marginLeft: 10, marginTop: 10}}>android</i>}
         />
       </div>
     );
   }
 }
+
+
+{/*iconElementLeft={<FlatButton label="Logout" style={{backgroundColor: '#5bc66f', borderColor: "#888", borderRadius: 5, borderStyle: 'ridge', borderWidth: 1, color: '#FFF', marginTop: 5}} />} */}
--- front-end/src/components/PostPreview.js
+++ front-end/src/components/PostPreview.js
@@ -5,6 +5,7 @@ class PostPreview extends React.Component {
   render() {
     return (
       <Card style={{backgroundColor: '#d7dee8'}}>
+          <CardText style={{fontWeight: 'bold', textDecoration: "underline"}}>Post Preview</CardText>
           <CardText style={{fontWeight: 'bold'}}>{this.props.title}</CardText>
           <CardText>{this.props.body}</CardText>
           <CardText>{this.props.url}</CardText>
